a2c57bc8f8bcb76226807c38202248b4278dcf5d
chevalier <chevalier.vladimir@gmail.com>
2021-11-15 00:54:03 +0200

    add new functional

--- force-app/main/default/lwc/fileUploadLWC/fileUploadLWC.js
+++ force-app/main/default/lwc/fileUploadLWC/fileUploadLWC.js
@@ -1,8 +1,7 @@
 import { LightningElement, track, wire, api } from 'lwc';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import saveFile from '@salesforce/apex/FileUploadController.saveFile';
 import getContactsRelatedToAccount from '@salesforce/apex/FileUploadController.getContactsRelatedToAccount';
-import csvFileRead from '@salesforce/apex/FileUploadController.csvFileRead';
-
 
 const columns = [
     { label: 'First Name', fieldName: 'FirstName', type: 'text' },
@@ -15,19 +14,23 @@ const columns = [
         value: 'view',  
         iconPosition: 'left'  
     }}
-    ];
+    ]; 
+
 
 export default class FileUploadLWC extends LightningElement {
 
     @api recordId;
-    @track error;
     @track data;
-    @track columns = columns;
 
-    get csvFormat() {
-        return ['.csv'];
-    }
-    
+    @track columns = columns;
+    @track error;
+
+    filesUploaded = [];
+    file;
+    fileContents;
+    fileReader;
+    content;
+
     @wire(getContactsRelatedToAccount, {accId: '$recordId'}) 
     WireContactRecords({error, data}){
         if(data){
@@ -38,74 +41,41 @@ export default class FileUploadLWC extends LightningElement {
             this.data = undefined;
         }
     }
-
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-    callRowAction( event ) {  
-          
-        const recId =  event.detail.row.Id;  
-        const actionName = event.detail.action.name;  
-        if ( actionName === 'Edit' ) {  
-  //TODO
-            // this[NavigationMixin.Navigate]({  
-            //     type: 'standard__recordPage',  
-            //     attributes: {  
-            //         recordId: recId,  
-            //         objectApiName: 'Account',  
-            //         actionName: 'edit'  
-            //     }  
-            // })  
-  
-        } else if ( actionName === 'View') {  
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'View!!',
-                    message: 'Congratulation!!!\n You press view button!!!!',
-                    variant: 'Info',
-                }));
-
-            // this[NavigationMixin.Navigate]({  
-            //     type: 'standard__recordPage',  
-            //     attributes: {  
-            //         recordId: recId,  
-            //         objectApiName: 'Account',  
-            //         actionName: 'view'  
-            //     }  
-            // })  
-  
-        }          
-  
-    }
-
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=
-    uploadFileHandler(event) {
-           
-        const uploadedFiles = event.detail.files;
-
+    
+    handleFilesChange(event) {
         
-        // csvFileRead({ contentDocumentId: uploadedFiles[0].documentI, recordId: this.recordId})
-        csvFileRead({recordId: this.recordId, contentDocumentId: uploadedFiles[0].documentId})
-        .then(result => {
-            window.console.log('result ===> '+result);
-            this.data = result;
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Success!!',
-                    message: 'Accounts are created according to the CSV file upload!!!',
-                    variant: 'Success',
-                }),
-            );
-        })
-        .catch(error => {
-            this.error = error;
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Error!!',
-                    message: JSON.stringify(error),
-                    variant: 'error',
-                }),
-            );     
-        })
+        this.file = event.target.files[0];
+        
 
-    }
+        this.fileReader = new FileReader();
+        this.fileReader.onload = (() => {
+        
+            this.fileContents = this.fileReader.result;
+            
+                saveFile({ base64Data: this.fileReader.result.split(',')[1], filename: this.file.name, accId: this.recordId })
+           .then(result => {
+                    window.console.log('result ====> ');
+                    window.console.log(result);
+                    this.data = result;
+                    this.fileName = this.fileName + ' - Uploaded Successfully';
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                        title: 'Success!!',
+                            message: this.file.name + ' - Uploaded Successfully!!!',
+                            variant: 'success',
+                        }),
+                    );
+                }).catch(error => {
+                    window.console.log(error);
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Error while uploading File',
+                            message: error.message,
+                            variant: 'error',
+                        }),
+                    );
+                });
+        });
+        this.fileReader.readAsDataURL(this.file);
+   }
 }
